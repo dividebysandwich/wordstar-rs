f2f2145f599ad8b5fa2e802ee1e1872e439b610c
dividebysandwich <josef.jahn@gmail.com>
2026-09-19 14:01:54 +0200

    Use is_multiple_of in digit grouping

--- src/attributes.rs
+++ src/attributes.rs
@@ -293,7 +293,7 @@ pub fn group_digits(n: usize) -> String {
     let digits = n.to_string();
     let mut out = String::new();
     for (i, c) in digits.chars().enumerate() {
-        if i > 0 && (digits.len() - i) % 3 == 0 {
+        if i > 0 && (digits.len() - i).is_multiple_of(3) {
             out.push(',');
         }
         out.push(c);
